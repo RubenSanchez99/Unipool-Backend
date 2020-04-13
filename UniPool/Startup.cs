34dd98c4f2b616e5dbf2905d672a0129a88c8c24
Rubén Sánchez <luisruben.sanchez99@gmail.com>
2020-04-13 17:47:07 -0500

    Added handler for DATABASE_URL

--- UniPool/Startup.cs
+++ UniPool/Startup.cs
@@ -30,7 +30,7 @@ namespace UniPool
         {
             services.AddControllers();
             services.AddMediatR(typeof(Startup));
-            services.AddDbContext<UniPoolContext>(options => options.UseNpgsql(Configuration["ConnectionString"]));
+            services.AddDbContext<UniPoolContext>(options => options.UseNpgsql(convertUrlConnectionString(Configuration["DATABASE_URL"])));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +51,21 @@ namespace UniPool
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private static string convertUrlConnectionString(string url)
+        {
+            if (!url.StartsWith("postgres")) return url;
+
+            var uri = new Uri(url);
+            var host = uri.Host;
+            var port = uri.Port;
+            var database = uri.Segments.Last();
+            var parts = uri.AbsoluteUri.Split(':', '/', '@');
+            var user = parts[3];
+            var password = parts[4];
+
+            return $"host={host}; port={port}; database={database}; username={user}; password={password}; SSL Mode=Prefer; Trust Server Certificate=true";
         }
     }
 }
